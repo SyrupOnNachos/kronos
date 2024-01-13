6ef7915394bc6c1796cfc63c7746bd91b73beb82
Austin <80305855+SyrupOnNachos@users.noreply.github.com>
2024-01-13 15:09:00 -0600

    added pipenv install to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ if [ "$ENV" == "local" ]
 then
     pipenv run uvicorn main:app --reload --port 80 --host 0.0.0.0
 else
+    pipenv install
     pipenv run uvicorn main:app --port $PORT --host 0.0.0.0
 fi
